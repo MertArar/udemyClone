3f30b4c986fa7ac7fc1fce7c57a79bb229a5784c
MertArar <mertarar.ma@gmail.com>
2023-06-09 04:33:04 +0300

    all actions added

--- src/action.js
+++ src/action.js
@@ -1,7 +1,9 @@
-import React from "react";
-
-const action = () => {
-  return <div>action</div>;
-};
-
-export default action;
+export const OPEN_SIDEBAR = "OPEN_SIDEBAR";
+export const CLOSE_SIDEBAR = "CLOSE_SIDEBAR";
+export const GET_COURSES = "GET_COURSES";
+export const GET_SINGLE_COURSE = "GET_SINGLE_COURSE";
+export const GET_CATEGORIES = "GET_CATEGORIES";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_CARD_ITEM = "REMOVE_CARD_ITEM";
+export const CLEAR_CART = "CLEAR_CART";
+export const GET_CART_TOTAL = "GET_CART_TOTAL";
