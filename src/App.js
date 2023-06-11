24c2ff45134b6259f75bb947a67ac68685f58228
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-11 05:37:48 +0300

    additional changes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, SingleCourse, Cart, Courses } from "./pages";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <div className="App">test</div>;
+  return <div>test</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { SidebarProvider } from "./context/sidebar_context";
+import { CoursesProvider } from "./context/courses_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
